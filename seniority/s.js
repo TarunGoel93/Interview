271f3f19f3f3b72fa2796b742620359c1e52aac0
ezCoder7 <162687566+ezCoder7@users.noreply.github.com>
2024-04-26 18:11:14 +0530

    s.js

---
+++ seniority/s.js
@@ -0,0 +1,24 @@
+let go=document.querySelector("#go");
+go.addEventListener("click",()=>{
+  window.location.href="http://127.0.0.1:5500/Form/form.html";
+});
+let b1=document.querySelector("#b1");
+let b2=document.querySelector("#b2");
+let b3=document.querySelector("#b3");
+let b4=document.querySelector("#b4");
+let b5=document.querySelector("#b5");
+b1.addEventListener("click",()=>{
+  b1.style.background="#D9D9D9";
+})
+b2.addEventListener("click",()=>{
+  b2.style.background="#D9D9D9";
+})
+b3.addEventListener("click",()=>{
+  b3.style.background="#D9D9D9";
+})
+b4.addEventListener("click",()=>{
+  b4.style.background="#D9D9D9";
+})
+b5.addEventListener("click",()=>{
+  b5.style.background="#D9D9D9";
+})
