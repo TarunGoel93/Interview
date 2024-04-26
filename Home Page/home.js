7f0ff2de4a174a50d9a369d6b17f2e19c74e6905
ezCoder7 <162687566+ezCoder7@users.noreply.github.com>
2024-04-26 18:07:01 +0530

    home.js

---
+++ Home Page/home.js
@@ -0,0 +1,108 @@
+let h=document.querySelector("#home");
+let p=document.querySelector("#profile");
+let how=document.querySelector("#conduct");
+h.addEventListener("mouseenter",()=>{
+  h.style.background="#393939";
+})
+h.addEventListener("mouseleave",()=>{
+  h.style.background="black";
+})
+p.addEventListener("mouseenter",()=>{
+  p.style.background="#393939";
+})
+p.addEventListener("mouseleave",()=>{
+  p.style.background="black";
+})
+how.addEventListener("mouseenter",()=>{
+  how.style.background="#393939";
+})
+how.addEventListener("mouseleave",()=>{
+  how.style.background="black";
+})
+let intern=document.querySelector("#intern");
+let acc=document.querySelector("#acc");
+let sycn=document.querySelector("#sycn");
+intern.addEventListener("mouseenter",()=>{
+  intern.style.background="#F8F8F8";
+})
+intern.addEventListener("mouseleave",()=>{
+  intern.style.background="white";
+})
+acc.addEventListener("mouseenter",()=>{
+  acc.style.background="#F8F8F8";
+})
+acc.addEventListener("mouseleave",()=>{
+  acc.style.background="white";
+})
+sycn.addEventListener("mouseenter",()=>{
+  sycn.style.background="#F8F8F8";
+})
+sycn.addEventListener("mouseleave",()=>{
+  sycn.style.background="white";
+})
+let b1=document.querySelector("#m1");
+let b2=document.querySelector("#m2");
+let b3=document.querySelector("#m3");
+let b4=document.querySelector("#m4");
+let b5=document.querySelector("#m5");
+let b6=document.querySelector("#m6");
+let b7=document.querySelector("#m7");
+let b8=document.querySelector("#m8");
+let b9=document.querySelector("#m9");
+b1.addEventListener("mouseenter",()=>{
+  b1.style.background="#D9D9D9";
+})
+b1.addEventListener("mouseleave",()=>{
+  b1.style.background="#F2F2F2";
+})
+b2.addEventListener("mouseenter",()=>{
+  b2.style.background="#D9D9D9";
+})
+b2.addEventListener("mouseleave",()=>{
+  b2.style.background="#F2F2F2";
+})
+b3.addEventListener("mouseenter",()=>{
+  b3.style.background="#D9D9D9";
+})
+b3.addEventListener("mouseleave",()=>{
+  b3.style.background="#F2F2F2";
+})
+b4.addEventListener("mouseenter",()=>{
+  b4.style.background="#D9D9D9";
+})
+b4.addEventListener("mouseleave",()=>{
+  b4.style.background="#F2F2F2";
+})
+b5.addEventListener("mouseenter",()=>{
+  b5.style.background="#D9D9D9";
+})
+b5.addEventListener("mouseleave",()=>{
+  b5.style.background="#F2F2F2";
+})
+b6.addEventListener("mouseenter",()=>{
+  b6.style.background="#D9D9D9";
+})
+b6.addEventListener("mouseleave",()=>{
+  b6.style.background="#F2F2F2";
+})
+b7.addEventListener("mouseenter",()=>{
+  b7.style.background="#D9D9D9";
+})
+b7.addEventListener("mouseleave",()=>{
+  b7.style.background="#F2F2F2";
+})
+b8.addEventListener("mouseenter",()=>{
+  b8.style.background="#D9D9D9";
+})
+b8.addEventListener("mouseleave",()=>{
+  b8.style.background="#F2F2F2";
+})
+b9.addEventListener("mouseenter",()=>{
+  b9.style.background="#D9D9D9";
+})
+b9.addEventListener("mouseleave",()=>{
+  b9.style.background="#F2F2F2";
+})
+m1.addEventListener("click",()=>{
+  window.location.href="http://127.0.0.1:5500/Frontend/f.html";
+});
